8e8c6bb907cabb13a279cd3ba65e7cf3c9e0b359
Manuel Maldonado <manypark@live.com>
2025-03-31 23:08:12 -0600

    feat: change watch bloc for BlocBuilder

--- lib/features/tasks/presentation/widgets/lists/list_tasks_widget.dart
+++ lib/features/tasks/presentation/widgets/lists/list_tasks_widget.dart
@@ -7,9 +7,8 @@ import 'package:bitacoras/features/tasks/domain/domain.dart';
 import 'package:bitacoras/features/tasks/presentation/blocs/blocs.dart';
 
 class ListTasksWidget extends StatelessWidget {
-
   final List<TasksEntity> tasks;
-  
+
   const ListTasksWidget({
     super.key,
     required this.tasks,
@@ -17,51 +16,46 @@ class ListTasksWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
-    final bool isLoading = context.watch<TasksBloc>().state.isLoading;
-
     return SizedBox(
-      width : double.infinity,
+      width: double.infinity,
       height: MediaQuery.sizeOf(context).height * 0.8,
-      child : Stack(
+      child: Stack(
         children: [
-      
+          
           SingleChildScrollView(
             child: Column(
               children: [
-          
-                ...tasks.map( (task) {
+                ...tasks.map((task) {
                   return Container(
-                    margin    : EdgeInsets.symmetric( horizontal: LayoutConstants.paddingL, vertical: LayoutConstants.paddingL ),
+                    margin: EdgeInsets.symmetric(
+                        horizontal: LayoutConstants.paddingL,
+                        vertical: LayoutConstants.paddingL),
                     decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      color       : Colors.white,
-                      boxShadow   : [
-                        BoxShadow(
-                          blurRadius: 5,
-                          color     : Colors.black12,
-                          offset    : Offset(0, 5)
-                        )
-                      ]
-                    ),
+                        borderRadius: BorderRadius.circular(20),
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(
+                              blurRadius: 5,
+                              color: Colors.black12,
+                              offset: Offset(0, 5))
+                        ]),
                     child: InkWell(
-                      onTap: () {
-                        
-                      },
+                      onTap: () {},
                       child: ListTile(
-                        title   : Text(task.title, style: GlobalFonts.paragraphBodyLargeBold,),
+                        title: Text(
+                          task.title,
+                          style: GlobalFonts.paragraphBodyLargeBold,
+                        ),
                         subtitle: Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            Text( task.description ),
-                            Chip( 
-                              label: Text(
-                                task.status, 
-                                style: GlobalFonts.paragraphBodyMediumBold.copyWith( 
-                                  color: Colors.red.shade300
-                                )
-                              ),
-                              backgroundColor: Colors.red.shade100, side: BorderSide.none,
+                            Text(task.description),
+                            Chip(
+                              label: Text(task.status,
+                                  style: GlobalFonts.paragraphBodyMediumBold
+                                      .copyWith(color: Colors.red.shade300)),
+                              backgroundColor: Colors.red.shade100,
+                              side: BorderSide.none,
                             ),
                           ],
                         ),
@@ -69,41 +63,45 @@ class ListTasksWidget extends StatelessWidget {
                     ),
                   );
                 }),
-          
               ],
             ),
           ),
-      
+          
           Positioned(
             bottom: MediaQuery.sizeOf(context).height * 0.06,
-            right : LayoutConstants.paddingL,
-            child : ElevatedButton(
-              onPressed : isLoading ? null : () => context.read<TasksBloc>().loadListTasks(),
-              child     : AnimatedSize(
-                duration: Duration(milliseconds: 300),
-                curve: Curves.easeInOut,
-                child: Row(
-                  children: [
-                
-                    AnimatedRotation(
-                      turns   : isLoading ? 1 : 0,
-                      duration: Duration(seconds: 1),
-                      curve   : Curves.linear,
-                      child   : const Icon(Icons.refresh, color: Colors.white),
-                    ),
+            right: LayoutConstants.paddingL,
+            child: BlocBuilder<TasksBloc, TasksState>(
+              builder: (context, state) {
 
-                
-                    if(!isLoading)
-                    SizedBox( width: LayoutConstants.spaceS ),
-                
-                    if(!isLoading)
-                    Text(
-                      'Actualizar tareas',
-                      style: GlobalFonts.paragraphBodyMediumRegular.copyWith( color: Colors.white )
-                    )
-                  ],
-                ),
-              ),
+                final isLoading = state.isLoading;
+
+                return ElevatedButton(
+                  onPressed : isLoading ? null: () => context.read<TasksBloc>().loadListTasks(),
+                  child     : AnimatedSize(
+                    duration: Duration(milliseconds: 300),
+                    curve   : Curves.easeInOut,
+                    child   : Row(
+                      children: [
+
+                        AnimatedRotation(
+                          turns: isLoading ? 1 : 0,
+                          duration: Duration(seconds: 1),
+                          curve: Curves.linear,
+                          child: const Icon(Icons.refresh, color: Colors.white),
+                        ),
+
+                        if (!isLoading) SizedBox(width: LayoutConstants.spaceS),
+
+                        if (!isLoading)
+                          Text(
+                            'Actualizar tareas',
+                            style: GlobalFonts.paragraphBodyMediumRegular.copyWith(color: Colors.white)
+                          )
+                      ],
+                    ),
+                  ),
+                );
+              },
             ),
           ),
         ],
